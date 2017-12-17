b00423a0be60d987f2d5f33fc8ed8aabe75633e0
Max Brosnahan <maximilianbrosnahan@gmail.com>
2017-12-17 14:33:36 -0700

    Setup page template

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,60 +6,17 @@ import Helmet from 'react-helmet'
 
 import './index.css'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
-
-const TemplateWrapper = ({ children }) => (
-  <div>
+const TemplateWrapper = ({ children }: {children: () => React.Node}) => (
+  <div className="template-wrapper">
     <Helmet
-      title="Gatsby Default Starter"
+      title="Web Rebels 2018"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Web Rebels JS conference 2018, Oslo' },
+        { name: 'keywords', content: 'webrebels, conference, javascript, js, olso' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
   </div>
 )
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
 
 export default TemplateWrapper
