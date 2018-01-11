87eacd5ac38eddf05e2257831e3fc6687479d724
Max Brosnahan <maximilianbrosnahan@gmail.com>
2018-01-11 23:03:02 +0100

    Tweak styling

--- src/components/panel/index.js
+++ src/components/panel/index.js
@@ -5,10 +5,10 @@ import "./index.scss";
 export default function Panel({ title, children }) {
   return (
     <div className="panel">
-      <h1>
+      <div className="title-wrapper">
         <div className="title-shape" />
-        {title}
-      </h1>
+        <h1>{title}</h1>
+      </div>
       <div className="contents">
         {children}
         <div className="right-border" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,24 +10,26 @@ const IndexPage = () => (
   <div className="page-index">
     <div className="image-wrapper">
       <img className="imagery" src={imageryUrl} alt="Oslo city scape" />
-      <Panel title="WR_2018_CALL_FOR_PROPOSALS">
-        <p>
-          <span>Submit your proposals before March 1. 2018</span>
-        </p>
-        <p>
-          <span>
-            The final speaker line-up will be a mix of invited speakers
-          </span>
-          <span>– and those accepted from the CfP.</span>
-        </p>
-        <p>
-          <span>We particularly encourage people new to public speaking</span>
-          <span>and members of underrepresented groups</span>
-          <span>in tech to send in their suggestions.</span>
-        </p>
+      <div className="panel-wrapper">
+        <Panel title="WR_2018_CALL_FOR_PROPOSALS">
+          <p>
+            <span>Submit your proposals before March 1. 2018</span>
+          </p>
+          <p>
+            <span>
+              The final speaker line-up will be a mix of invited speakers
+            </span>
+            <span>– and those accepted from the CfP.</span>
+          </p>
+          <p>
+            <span>We particularly encourage people new to public speaking</span>
+            <span>and members of underrepresented groups</span>
+            <span>in tech to send in their suggestions.</span>
+          </p>
 
-        <Button to="https://www.papercall.io/webreb2018">Submit CFP</Button>
-      </Panel>
+          <Button to="https://www.papercall.io/webreb2018">Submit CFP</Button>
+        </Panel>
+      </div>
     </div>
     <footer>
       Want to sponsor us? Head over to <a href="/sponsors">sponsors</a>.
