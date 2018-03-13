c51dc06b5dc2ed2af1b1c3d1675805aafb1e3ba4
Max Brosnahan <maximilianbrosnahan@gmail.com>
2018-03-13 11:14:38 +0100

    Remove CFP
    Why: It has closed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,25 +26,6 @@ const IndexPage = () => (
           </p>
           <SignupForm />
         </Panel>
-
-        <Panel title={<h1>CALL_FOR_PROPOSALS</h1>}>
-          <p>
-            <span>Submit your proposals before February 25. 2018</span>
-          </p>
-          <p>
-            <span>
-              The final speaker line-up will be a mix of invited speakers
-            </span>
-            <span>– and those accepted from the CfP.</span>
-          </p>
-          <p>
-            <span>We particularly encourage people new to public speaking</span>
-            <span>and members of underrepresented groups</span>
-            <span>in tech to send in their suggestions.</span>
-          </p>
-
-          <Button to="https://www.papercall.io/webreb2018">Submit CFP</Button>
-        </Panel>
       </div>
     </div>
   </div>
