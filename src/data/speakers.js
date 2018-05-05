4bc85f74adfcbe8a3aa244bf0d95c5e5105b3b34
Trygve Lie <post@trygve-lie.com>
2018-05-05 09:07:03 +0200

    More speakers

--- src/data/speakers.js
+++ src/data/speakers.js
@@ -2,8 +2,119 @@ import martinSplit from '../graphics/speaker_martin_split.jpg';
 import maximSalnikov from '../graphics/speaker_maxim_salnikov.jpg';
 import michaelaLehr from '../graphics/speaker_michaela_lehr.jpg';
 import michaelGeers from '../graphics/speaker_michael_geers.jpg';
+import johnPapa from '../graphics/speaker_john_papa.jpg';
+import ernieTurner from '../graphics/speaker_ernie_turner.jpg';
+import joeKarlsson from '../graphics/speaker_joe_karlsson.jpg';
+import lucianoMammino from '../graphics/speaker_luciano_mammino.jpg';
+import madlainaKalunder from '../graphics/speaker_madlaina_kalunder.jpg';
+import monikaKedrova from '../graphics/speaker_monika_kedrova.jpg';
+import sigurdSchneider from '../graphics/speaker_sigurd_schneider.jpg';
+import p01 from '../graphics/speaker_p01.jpg';
+
+
 
 export default [
+  {
+    name: 'p01',
+    bio: [
+      'P01 worked for Opera Software for 11 years, but are now part of the Microsoft Office 365 team. In his spare time he makes unbelivable crazy tiny demos in JavaScript.'
+    ],
+    title: 'A testing story in 3 acts',
+    abstract: [
+      'Testing is a solved problem they say. You probably saw talks about it: About the good old dogs and the new kid on the block.',
+      'But does it apply to your project? How to test if you deliver a third party component used in many workloads with different integration strategies?'
+    ],
+    photoUrl: p01
+  },
+
+  {
+    name: 'Sigurd Schneider',
+    bio: [
+      'V8 core team developer with a background in programming languages and compiler verification.'
+    ],
+    title: 'Supporting modern JavaScript idioms for array builtins in V8',
+    abstract: [
+      'From a high-level we will look at how V8 ensures performance for array builtins behind the scenes. We will see that V8 has to make certain assumptions to be able to generate optimized code. I’ll explain how V8 decides which assumptions to make, what happens if an assumption turns out to be wrong, and how V8 learns from making wrong assumptions.',
+      'This knowledge leads to useful insights for JavaScript developers wishing to improve their code’s run-time performance.'
+    ],
+    photoUrl: sigurdSchneider
+  },
+
+  {
+    name: 'Monika Kedrova',
+    bio: [
+      'Monika is a full stack developer who loves challenges - whether it’s solving problems of all kinds, learning new things, or climbing mountains. Her day job involves writing server side code in Scala and taking care of infrastructure and databases. She likes to keep it fun by making WebXR demos and sharing the joy of VR at conferences. She’s a huge fan of functional programming and well written documentation.'
+    ],
+    title: 'Building virtual worlds with web technologies',
+    abstract: [
+      'We will explore the landscape of virtual reality on the web, look at where it started and where it’s heading and then we’ll dive into how to create a multi-user interactive experience available on all platforms. For this we’ll use Mozilla’s A-Frame library, which makes it easy and fun to get into WebXR. We will have a look at how to compose scenes with HTML-like syntax from a range of components and how to modify them in the in-browser editor.'
+    ],
+    photoUrl: monikaKedrova
+  },
+
+  {
+    name: 'Madlaina Kalunder',
+    bio: [
+      'Madlaina is a full stack developer and a 3d graphics enthusiast on a mission to make 3d content accessible for people working on the web.',
+      'She graduated at the Zurich University of Arts with a bachelor degree in Design, specialization Game Design, combining the passion for design and storytelling with the newest technologies.',
+      'Madlaina believes in bringing people of different backgrounds together to solve problems in creative ways.'
+    ],
+    title: '',
+    abstract: [
+
+    ],
+    photoUrl: madlainaKalunder
+  },
+
+  {
+    name: 'Luciano Mammino',
+    bio: [
+      'Luciano is a software engineer born in 1987, the same year that “Super Mario Bros” was released in Europe, which, by chance is his favourite game! He started coding early at the age of 12, hacking away with his father`s old i386 armed only with MS-DOS and the QBasic interpreter.',
+      'He has been professionally a software developer for more than 10 years and are currently a Solution Architect at Vectra AI in Dublin where he is working on automating the hunt for cyberattackers and speeding-up incident response.'
+    ],
+    title: 'Cracking JWT tokens: a tale of magic, Node.JS and parallel computing',
+    abstract: [
+      'JWT tokens are powerful artifacts that magicians from all over the World are using to secure modern apps and APIs. But as any other magic tool, those powerful tokens need to be used with caution and mastery, otherwise, they might become dangerous exploitable vulnerabilities. In this talk, I will demonstrate how evil magicians can cast a JWT brute force spell to take control of poorly constructed JWT tokens.'
+    ],
+    photoUrl: lucianoMammino
+  },
+
+  {
+    name: 'Joe Karlsson',
+    bio: [
+      'JavaScript Engineer at Best Buy. Builder of weird software. Co-Creator of bechdel.io.'
+    ],
+    title: 'Building High Performance React Applications',
+    abstract: [
+      'React is built with performance in mind. But when is React slow? In this talk we’ll discuss common bottlenecks in React and when you might be making your program work harder than it should. We will discuss how Best Buy builds components that stay fast, even during the enormous stress of Black Friday traffic. You will learn practical ways to speed up your real world React applications today.'
+    ],
+    photoUrl: joeKarlsson
+  },
+
+  {
+    name: 'Ernie Turner',
+    bio: [
+      'Ernie has over ten years of experience building Enterprise web applications and developer platforms going back to the dark, soul crushing days of IE6. Ernie now works at IronCore Labs building turnkey data security for developers so they can focus on their core features and still get strong security.',
+      'When not making the digital world a better place, he enjoys spending time in the great outdoors with his family in beautiful Montana.'
+    ],
+    title: 'Dodging Web Crypto API Landmines',
+    abstract: [
+      'A look at how to use the Web Crypto API within a production application. By focusing on the practical implementation of cryptography and not on cryptography generalities we will see that cryptography is not as scary a concept as you might think.'
+    ],
+    photoUrl: ernieTurner
+  },
+
+  {
+    name: 'John Papa',
+    bio: [
+      'John Papa is dedicated a father and husband, a Principal Developer Advocate with Microsoft, and alumnus of the Google Developer Expert, Microsoft RD and MVP programs.',
+      'His passions are deploying and teaching modern web technologies and enjoying everything Disney with his family. John is a co-host of the popular Adventures in Angular podcast, author of the Angular Style Guide, and many popular Pluralsight courses.'
+    ],
+    title: '',
+    abstract: [],
+    photoUrl: johnPapa
+  },
+
   {
     name: 'Martin Splitt',
     bio: [
