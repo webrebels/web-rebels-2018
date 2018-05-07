489947e5287b1748d56afdb4c922e1276869c4f7
Trygve Lie <post@trygve-lie.com>
2018-05-08 01:21:00 +0200

    Added speaker

--- src/data/speakers.js
+++ src/data/speakers.js
@@ -12,9 +12,25 @@ import sigurdSchneider from '../graphics/speaker_sigurd_schneider.jpg';
 import p01 from '../graphics/speaker_p01.jpg';
 import stefanJudis from '../graphics/speaker_stefan_judis.jpg';
 import ivanJovanovic from '../graphics/speaker_ivan_jovanovic.jpg';
+import emilBay from '../graphics/speaker_emil_bay.jpg';
 
 
 export default [
+  {
+    name: 'Emil Bay',
+    bio: [
+      'Emil Bay is a independent consultant from Copenhagen, Denmark. He used to study maths, but quickly dropped out to work on software. He has worked at a biomedical instrument lab, HPC lab, an agency and a newspaper, as a data journalist.',
+      'He currently works on making cryptography and security primitives more accessible.'
+    ],
+    title: 'Hand-crafting WebAssembly',
+    abstract: [
+      'There has been many talks about what WebAssembly (WASM) is, its relation to compilers and how bright our collective future is with WASM in our toolbox. However most talks treat WebAssembly as a semi-opaque box, and mostly as something you can compile higher level languages into. This is a pity! WebAssembly is a fun language to learn, and allows one to write code often less than a magnitude slower than C!',
+      'In this talk I will show how to write WAT (WebAssembly Text-format), how to reason about algorithms when all you have is one large slab of memory, how to convert high level constructs such as loops into elementary instructions and how to have fun at the same time! We will convert a number of progressively harder algorithms, each revealing a challenging aspect of working with no abstractions.',
+      'Even if you will not write WASM at work, learning at the lowest level of computer tears away the enchantment that abstractions cause, and reveals the magical machine that is the computer.'
+    ],
+    photoUrl: emilBay
+  },
+
   {
     name: 'Ivan Jovanovic',
     bio: [
