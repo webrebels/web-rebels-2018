775a2dead9903e4a4b3582efce1625f269a25710
Anders Olsen Sandvik <288977+Andersos@users.noreply.github.com>
2018-06-04 09:02:53 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = () => (
           <iframe
             class="livestream"
             height="315"
-            src="https://www.youtube.com/watch?v=xP1kbqn3ISo"
+            src="https://www.youtube.com/embed/xP1kbqn3ISo"
             frameborder="0"
             allowfullscreen
           />
